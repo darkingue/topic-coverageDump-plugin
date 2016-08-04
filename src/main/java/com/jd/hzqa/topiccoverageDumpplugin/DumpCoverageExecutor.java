17f7037c002aa21c025efae60d869c7042138147
qqs <wyqianqingsong@jd.com>
2016-08-04 10:35:04 +0800

    完善功能

--- src/main/java/com/jd/hzqa/topiccoverageDumpplugin/DumpCoverageExecutor.java
+++ src/main/java/com/jd/hzqa/topiccoverageDumpplugin/DumpCoverageExecutor.java
@@ -1,10 +1,6 @@
 package com.jd.hzqa.topiccoverageDumpplugin;
 
 import com.wangyin.qa.tools.jacoco.JacocoUtil;
-import hudson.model.Job;
-
-import java.io.File;
-import java.io.FilenameFilter;
 
 /**
  * Created by qqs on 15/7/15.
@@ -13,10 +9,8 @@ public class DumpCoverageExecutor {
 
     public static boolean dumpJaCoCoReport(String jobFile, String agentIP, int agentPort) {
 
-        boolean dumpSuccess = false;
-        if (JacocoUtil.dumpIsOk(jobFile, agentIP, agentPort)) {
-            dumpSuccess = true;
-        }
+        boolean dumpSuccess = JacocoUtil.dumpIsOk(jobFile, agentIP, agentPort);
+
         return dumpSuccess;
 
     }
